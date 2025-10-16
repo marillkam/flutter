58d8cd64fc6a9f4699bc3852cf3491acf2d932ba
marillkam <164666802+marillkam@users.noreply.github.com>
2025-10-16 13:21:22 +0000

    changes in code + image

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -11,21 +11,46 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My App'),
+        ),
         body: Center(
-          child: Container(
-            padding: const EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: Color.fromARGB(54, 206, 106, 181),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: const Text(
-              'Hello World!',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 24,
+          child: Column(
+            children: [
+              const CircleAvatar(
+                radius: 50,
+                backgroundImage: NetworkImage(
+                    'https://avatars.mds.yandex.net/i?id=204870453fe1b245ce676003a0dc0766_sr-5233839-images-thumbs&n=13'),
               ),
-            ),
+              const SizedBox(height: 20),
+              Container(
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(153, 255, 63, 159),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Text(
+                  'Hello World!',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              const Expanded(
+                child: Center(
+                  child: Text(
+                    'ВСЕМ ПРИВЕТ',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ),
+            ],
           ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.add),
         ),
       ),
     );
